e265bf6cb6f1470c9ceae6c6d68285a71ca58d98
Full Stack Dev(Web &  Mobile, Blockchain) <Goodman36428@gmail.com>
2025-04-08 21:20:58 -0700

    update  4.8 9:20

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use flate2::read::GzDecoder;
 use serde_json::Value;
-use std::env; // Importing env module to get system path
+use std::env;                                                                         // Importing env module to get system path
 use std::fs::File;
 use std::io::{self, Read};
 
@@ -21,8 +21,8 @@ fn main() -> io::Result<()> {
 fn read_gz(file_path: &str) -> io::Result<Value> {
     let file = File::open(file_path)?;
     let mut decoder = GzDecoder::new(file);
-    let mut contents = Vec::new(); // Read as bytes
-    decoder.read_to_end(&mut contents)?; // Read the entire .gz file into bytes
+    let mut contents = Vec::new();                                                   // Read as bytes
+    decoder.read_to_end(&mut contents)?;                                             // Read the entire .gz file into bytes
 
     // Attempt to decode JSON from bytes
     match serde_json::from_slice(&contents) {
@@ -30,7 +30,7 @@ fn read_gz(file_path: &str) -> io::Result<Value> {
         Err(e) => {
             // Log the error and the raw contents for debugging
             println!("Failed to decode JSON: {}", e);
-            println!("Raw contents (first 100 bytes): {:?}", &contents[..100]); // Show first 100 bytes
+            println!("Raw contents (first 100 bytes): {:?}", &contents[..100]);     // Show first 100 bytes
             return Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 "Invalid JSON data",
